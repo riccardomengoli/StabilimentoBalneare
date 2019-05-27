00ef8a03ac796a161bd8c66112f5f2e5c8787661
lorenzo <lorenzo@lorenzo-pc>
2019-05-27 13:00:55 +0200

    IViewTermina Interfaccia Termina

---
+++ StabilimentoBalneare/src/ombrelloniani/view/IViewTermina.java
@@ -0,0 +1,17 @@
+package ombrelloniani.view;
+
+import java.util.List;
+
+public interface IViewTermina {
+
+	void aggiornaListaPrenotazioniDisponibili(List<String[]> prenotazioni);
+
+	void setId(String string);
+
+	void setDataInizio(String format);
+
+	void setDataFine(String format);
+
+	void aggiornaTabellaRicevuta(List<String> ricevuta, int numeroEntry);
+
+}
