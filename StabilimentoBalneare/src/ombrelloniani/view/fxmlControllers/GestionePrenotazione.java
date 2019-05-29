1e3368d22e5eb40e6a0d4ef90d63477666aa985e
riccardomengoli <mengoli.riccardo@gmail.com>
2019-05-30 00:43:23 +0200

    Added dynamic horizontal line in view

--- StabilimentoBalneare/src/ombrelloniani/view/fxmlControllers/GestionePrenotazione.java
+++ StabilimentoBalneare/src/ombrelloniani/view/fxmlControllers/GestionePrenotazione.java
@@ -1,9 +1,12 @@
 package ombrelloniani.view.fxmlControllers;
 
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
 import ombrelloniani.view.VistaNavigator;
 
 public class GestionePrenotazione extends FXMLController {
@@ -12,73 +15,86 @@ public class GestionePrenotazione extends FXMLController {
 	@FXML private Button modificaPren;
 	@FXML private Button terminaPren;
 	@FXML private Button cancellaPren;
+	@FXML private Line hr1;
 
-	public GestionePrenotazione() {};
-	
+	public GestionePrenotazione() {
+	};
+
 	/**
-	 * Evento al click del bottone di nuova prenotazione.
-	 * Cambio scena.
+	 * Lega la lunghezza della linea di separazione alla lunghezza del suo
+	 * container, necessario in quanto gli oggetti Shape non sono resizable. Vengono
+	 * aggiunti 0.5 px alla coordinata y per ottenere un effetto di pixel pieno,
+	 * evitando aliasing (Javadoc Shape). Initialize viene invocato al termine del
+	 * caricamento del contenuto del file fxml.
 	 */
 	@FXML
-	private void handleNuovaPrenButtonClick (ActionEvent event) throws Exception {
-		
-		//VistaNavigator.loadView(VistaNavigator.CREAPRENOTAZIONE);
+	private void initialize() {
+		Pane parent = (Pane) hr1.getParent();
+		hr1.startXProperty().bind(new SimpleDoubleProperty(50));
+		hr1.endXProperty().bind(parent.widthProperty().subtract(50));
+		// Evito aliasing
+		hr1.setStartY(0.5);
+		hr1.setEndY(0.5);
 	}
-	
+
 	/**
-	 * Evento al click del bottone di modifica prenotazione.
-	 * Cambio scena.
+	 * Evento al click del bottone di nuova prenotazione. Cambio scena.
 	 */
 	@FXML
-	private void handleModificaPrenButtonClick (ActionEvent event) throws Exception {
-		
-		//VistaNavigator.loadView(VistaNavigator.MODIFICAPRENOTAZIONE);
+	private void handleNuovaPrenButtonClick(ActionEvent event) throws Exception {
+
+		// VistaNavigator.loadView(VistaNavigator.CREAPRENOTAZIONE);
 	}
-	
+
 	/**
-	 * Evento al click del bottone di termina prenotazione.
-	 * Cambio scena.
+	 * Evento al click del bottone di modifica prenotazione. Cambio scena.
 	 */
 	@FXML
-	private void handleTerminaPrenButtonClick (ActionEvent event) throws Exception {
-		
-		//VistaNavigator.loadView(VistaNavigator.TERMINAPRENOTAZIONE);
+	private void handleModificaPrenButtonClick(ActionEvent event) throws Exception {
+
+		// VistaNavigator.loadView(VistaNavigator.MODIFICAPRENOTAZIONE);
 	}
-	
+
 	/**
-	 * Evento al click del bottone di cancella prenotazione.
-	 * Cambio scena.
+	 * Evento al click del bottone di termina prenotazione. Cambio scena.
 	 */
 	@FXML
-	private void handleCancellaPrenButtonClick (ActionEvent event) throws Exception {
-		
-		//VistaNavigator.loadView(VistaNavigator.CANCELLAPRENOTAZIONE);
+	private void handleTerminaPrenButtonClick(ActionEvent event) throws Exception {
+
+		// VistaNavigator.loadView(VistaNavigator.TERMINAPRENOTAZIONE);
 	}
-	
+
 	/**
-	 * Evento all'entrata del mouse sopra i bottoni del menu.
-	 * Imposta il colore.
+	 * Evento al click del bottone di cancella prenotazione. Cambio scena.
 	 */
 	@FXML
-	private void handleHoverInButton (MouseEvent event) {
+	private void handleCancellaPrenButtonClick(ActionEvent event) throws Exception {
+
+		// VistaNavigator.loadView(VistaNavigator.CANCELLAPRENOTAZIONE);
+	}
+
+	/**
+	 * Evento all'entrata del mouse sopra i bottoni del menu. Imposta il colore.
+	 */
+	@FXML
+	private void handleHoverInButton(MouseEvent event) {
 		((Button) event.getSource()).setStyle("-fx-background-color: #4449d9;");
 	}
-	
+
 	/**
-	 * Evento all'uscita del mouse dai bottoni del menu.
-	 * Resetta il colore principale.
+	 * Evento all'uscita del mouse dai bottoni del menu. Resetta il colore
+	 * principale.
 	 */
 	@FXML
-	private void handleHoverOutButton (MouseEvent event) {
+	private void handleHoverOutButton(MouseEvent event) {
 		((Button) event.getSource()).setStyle("");
 	}
-	
+
 	/**
-	 * Evento al click di indietro.
-	 * Ritorno alla view precedente.
+	 * Evento al click di indietro. Ritorno alla view precedente.
 	 */
 	@FXML
-	private void handleBackButtonClick (ActionEvent event) throws Exception {
+	private void handleBackButtonClick(ActionEvent event) throws Exception {
 		if (getCallingScene() != null) {
 			VistaNavigator.loadView(getCallingScene());
 		}
