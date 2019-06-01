7285499845c662f484ebbfc4e58dc09589a805a4
michelefilippini1997 <michele.filippini.01@gmail.com>
2019-06-01 19:10:16 +0200

    Modifica controllerTermina, tolti i throw e tolti i parametri dai metodi

--- StabilimentoBalneare/src/ombrelloniani/controller/interfaces/IControllerTermina.java
+++ StabilimentoBalneare/src/ombrelloniani/controller/interfaces/IControllerTermina.java
@@ -1,18 +1,16 @@
 package ombrelloniani.controller.interfaces;
 
-import java.text.ParseException;
-
 public interface IControllerTermina {
 
 	public void terminaPrenotazione();
 		
-	public void cercaPrenotazione(int idPren) throws ParseException;
+	public void cercaPrenotazione(int idPren);
 	
-	public void prenotazioneSelezionata(int index) throws ParseException;
+	public void prenotazioneSelezionata(int index);
 	
-	public void cercaPrenotazioni(String nome, String cognome) throws ParseException;
+	public void cercaPrenotazioni(String nome, String cognome);
 	
 	public void mostraConvenzioni();
 	
-	public void convenzioneSelezionata(int index) throws ParseException;
+	public void convenzioneSelezionata(int index);
 }
